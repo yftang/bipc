000b2b252b357afa2d867d50f200ffe6969ffb9e
Yifei Tang <tangyifei.tang@gmail.com>
2014-08-09 17:42:52 +0800

    Fix custome(r) typo and add simple edit and show page for Customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,26 @@
 class CustomersController < ApplicationController
+  before_filter :set_customer, only: [:new, :show, :edit, :destroy, :update]
+
   def index
     @customers = Customer.all
   end
 
   def new
-    @customer = Customer.new
+  end
+
+  def show
   end
 
   def edit
+  end
+
+  def update
+    if @customer.update_attributes(customer_params)
+      flash[:success] = "Customer updated"
+      redirect_to @customer
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -26,5 +39,9 @@ class CustomersController < ApplicationController
   def customer_params
     params.require(:customer).permit(:name, :email, :tel,
                                      :ins, :add, :city)
+  end
+
+  def set_customer
+    @customer = Customer.where(id: params[:id]).first || Customer.new
   end
 end
