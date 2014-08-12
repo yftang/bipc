4f6344a9db929915a9269b5a5a1221331aecb0a8
Yifei Tang <tangyifei.tang@gmail.com>
2014-08-12 13:00:27 +0800

    Create devise user model and simple actions and views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,5 @@
 class CustomersController < ApplicationController
+  before_filter :authenticate_user!
   before_filter :set_customer, only: [:show, :edit, :destroy, :update]
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources  :users
   root 'homepages#welcome'
   get 'homepages/welcome' => 'homepages#welcome'
   resources :projects do
