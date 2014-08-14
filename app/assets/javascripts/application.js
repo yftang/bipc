dc1d5fcd305d2d6fe743bf4f1ba7fe6ef7fff899
Yifei Tang <tangyifei.tang@gmail.com>
2014-08-14 13:01:57 +0800

    Add jumbotron to all pages, and remove turbolinks to address the 'rails-ujs has been loaded' problem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require_tree .
 $(document).ready(function() {
   $('.alert').fadeOut(3000);
