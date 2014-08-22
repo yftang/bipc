15782fd83f8e3c9500d2f8d4ff1408b6b4234eac
Yifei Tang <tangyifei.tang@gmail.com>
2014-08-22 11:08:32 +0800

    Add helper to show user roles tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,28 @@ module ApplicationHelper
     @title_name ? "#{@title_name} - #{base}" : base
   end
 
+  def role_tag(role)
+    role = role.to_s.camelize
+    gliph_span = "<span class='glyphicon glyphicon-user'></span>" if \
+        role.include?('Admin')
+    btn_base = "<button type='button' class='btn btn-sm btn-primary' " + \
+               "disabled='disabled'>#{gliph_span} %s</button>"
+
+    if role.include?('Marketing')
+      return btn_base % 'MKT'
+    elsif role.include?('Salesman')
+      return btn_base % 'SMN'
+    elsif role.include?('Experimenter')
+      return btn_base % 'EXP'
+    elsif role.include?('Bioinformatician')
+      return btn_base % 'BIN'
+    elsif role.include?('Admin')
+      return btn_base % 'ADM'
+    else
+      return
+    end
+  end
+
   def has_error?(object, field)
     "has-error" if object.errors.try(:messages).has_key?(field)
   end
