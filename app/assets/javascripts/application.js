b671a44bf75c7e9d1b0a7cfb357efc584e9616d4
Yifei Tang <tangyifei.tang@gmail.com>
2014-09-26 13:41:28 +0800

    Implementing samples destroy action event

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -52,6 +52,14 @@ $(document).ready(function() {
     });
   });
 
+  $.each($("table#samples-table input[type='checkbox']"), function(i,e) {
+    $(this).change(function() {
+      if($(this).is(":checked")) {
+        $("button#destroy-samples").attr("disabled", false);
+      }
+    });
+  });
+
   $("form#sign_in_user")
     .bind("ajax:success", function(e, data, status, xhr) {
       if(data.success == true) {
@@ -140,6 +148,22 @@ function destroy_customers() {
       $.ajax({
         type: "DELETE",
         url:  "/customers/"+customer_id
+      });
+    });
+    location.reload();
+  }
+}
+
+function destroy_samples() {
+  selected_checks = $("table#samples-table input:checked");
+  confirmed = confirm("Are you sure to remove thoses " +
+                      selected_checks.length+" sample(s)?")
+  if(confirmed == true) {
+    $.each(selected_checks, function(i, e) {
+      sample_id = $(this).val();
+      $.ajax({
+        type: "DELETE",
+        url:  "/samples/"+sample_id
       });
     });
     location.reload();
