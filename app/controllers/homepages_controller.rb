c7601e467f8b97f3f2ea3357af6889eb0e0b77e2
Yifei Tang <tangyifei.tang@gmail.com>
2014-10-13 15:39:42 +0800

    Redesign landing page for signed users

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,5 +1,11 @@
 class HomepagesController < ApplicationController
   def welcome
     @title = 'Home'
+    if user_signed_in? && current_user.role?(:admin)
+      @projects = Project.all
+    elsif user_signed_in?
+      @user_projects = current_user.projects
+      @ongoing_projects = @user_projects.reject(&:complete?)
+    end
   end
 end
